8447762804d560d102d5b54b2b5eeee0ccbabfcd
Alexander Fahlke <alexander.fahlke+git@gmail.com>
2020-02-11 00:22:21 +0100

    20 implemented Pearson16 and tests

--- hash/pearson/pearson.go
+++ hash/pearson/pearson.go
@@ -3,13 +3,33 @@ package pearson
 import "hash"
 
 const (
-	size      = 8
+	size      = 2
 	blockSize = 1
 )
 
-type digest uint64
+type digest uint16
 
-func New() hash.Hash64 {
+// 256-byte lookup table containing a permutation of the values 0 through 255.
+// https://www.random.org/sequences/?min=0&max=255&col=16&format=plain&rnd=new
+//nolint:gochecknoglobals
+var lookupTable = []uint8{165, 199, 36, 25, 48, 88, 201, 164, 173, 226, 211, 225, 103, 66, 208, 62,
+	252, 169, 251, 130, 162, 116, 133, 212, 12, 239, 218, 58, 113, 135, 9, 17,
+	151, 215, 221, 27, 45, 21, 46, 138, 235, 118, 111, 245, 214, 178, 224, 213,
+	246, 193, 69, 126, 167, 216, 244, 158, 41, 247, 198, 210, 137, 160, 86, 4,
+	189, 7, 197, 176, 35, 84, 129, 49, 231, 128, 78, 217, 23, 51, 29, 153,
+	180, 24, 19, 11, 161, 229, 237, 186, 33, 97, 219, 71, 77, 108, 52, 102,
+	233, 174, 70, 53, 14, 125, 99, 144, 123, 166, 195, 209, 223, 110, 106, 112,
+	172, 85, 6, 94, 131, 100, 15, 124, 243, 18, 156, 38, 73, 32, 175, 20,
+	114, 127, 91, 204, 242, 119, 68, 141, 206, 157, 168, 96, 57, 43, 76, 250,
+	150, 182, 98, 122, 238, 56, 145, 241, 81, 60, 143, 171, 115, 163, 44, 196,
+	75, 253, 187, 40, 61, 177, 8, 109, 28, 202, 31, 117, 134, 92, 188, 90,
+	54, 227, 191, 222, 26, 203, 146, 120, 184, 64, 30, 101, 170, 2, 154, 107,
+	59, 65, 10, 22, 1, 236, 230, 39, 87, 228, 248, 205, 95, 79, 132, 254,
+	0, 136, 207, 232, 152, 220, 147, 183, 47, 149, 67, 139, 192, 55, 121, 255,
+	104, 200, 249, 159, 240, 234, 148, 181, 82, 105, 142, 190, 3, 89, 42, 74,
+	72, 5, 13, 16, 37, 185, 34, 93, 155, 140, 179, 83, 80, 50, 63, 194}
+
+func New() hash.Hash {
 	d := digest(0)
 	d.Reset()
 
@@ -29,13 +49,26 @@ func (d *digest) Reset() {
 }
 
 func (d *digest) Sum(b []byte) []byte {
-	panic("implement me")
-}
+	s := uint16(*d)
 
-func (d *digest) Sum64() uint64 {
-	panic("implement me")
+	return append(b, byte(s>>8), byte(s)) //nolint:gomnd
 }
 
 func (d *digest) Write(p []byte) (n int, err error) {
-	panic("implement me")
+	var h, hh uint8
+
+	for i := range p {
+		switch i {
+		case 0:
+			h = lookupTable[p[0]]
+			hh = lookupTable[(p[0]+1)%255]
+		default:
+			h = lookupTable[h^p[i]]
+			hh = lookupTable[hh^p[i]]
+		}
+	}
+
+	*d = digest((uint16(h) << 8) | uint16(hh))
+
+	return len(p), nil
 }
--- hash/pearson/pearson_test.go
+++ hash/pearson/pearson_test.go
@@ -1,6 +1,7 @@
 package pearson
 
 import (
+	"encoding/binary"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,18 +10,24 @@ import (
 type test struct {
 	name   string
 	in     string
-	digest uint64
+	digest uint16
 }
 
 //nolint:gochecknoglobals
-var golden = []test{}
+var golden = []test{
+	{"empty string", "", 0x0},
+	{"foo", "foo", 0xad5c},
+	{"bar", "bar", 0x9be8},
+	{"baz", "baz", 0x488b},
+	{"Rainbow unicorn", "🦄🌈", 0x45ce},
+}
 
 func TestSize(t *testing.T) {
 	t.Parallel()
 
 	var d = New()
 
-	assert.Equal(t, 8, d.Size())
+	assert.Equal(t, 2, d.Size())
 }
 
 func TestBlockSize(t *testing.T) {
@@ -31,7 +38,31 @@ func TestBlockSize(t *testing.T) {
 	assert.Equal(t, 1, d.BlockSize())
 }
 
-func TestReset(t *testing.T) { t.Parallel() }
-func TestSum(t *testing.T)   { t.Parallel() }
-func TestSum32(t *testing.T) { t.Parallel() }
-func TestWrite(t *testing.T) { t.Parallel() }
+func TestReset(t *testing.T) {
+	t.Parallel()
+
+	var digest = New()
+
+	digest.Write([]byte("dummy")) //nolint:errcheck
+	digest.Reset()
+
+	assert.Equal(t, uint16(0x0), binary.BigEndian.Uint16(digest.Sum(nil))) //nolint:gomnd
+}
+
+func TestSum(t *testing.T) { t.Parallel() }
+
+func TestWrite(t *testing.T) {
+	t.Parallel()
+
+	for _, tt := range golden {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			var digest = New()
+
+			digest.Write([]byte(tt.in)) //nolint:errcheck
+			assert.Equal(t, tt.digest, binary.BigEndian.Uint16(digest.Sum(nil)))
+		})
+	}
+}
