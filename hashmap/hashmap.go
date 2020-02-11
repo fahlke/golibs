e71c6ede9804d681c5fd7c6c83b33956ad68c434
Alexander Fahlke <alexander.fahlke+git@gmail.com>
2020-02-11 20:12:17 +0100

    #8 added empty Set function

--- hashmap/hashmap.go
+++ hashmap/hashmap.go
@@ -1,1 +1,24 @@
 package hashmap
+
+import (
+	"hash"
+	"sync"
+)
+
+// maximum number of buckets possible with a Pearson-16 hash
+const buckets = 1 << 16
+
+type HashMap struct {
+	mutex sync.RWMutex
+	data  [buckets][]item
+	hash  hash.Hash
+}
+
+type item struct {
+	key   string
+	value interface{}
+}
+
+func (h *HashMap) Set(key string, value interface{}) error {
+	return nil
+}
--- hashmap/hashmap_test.go
+++ hashmap/hashmap_test.go
@@ -1,1 +1,18 @@
 package hashmap_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/fahlke/golibs/hashmap"
+)
+
+func TestSet(t *testing.T) {
+	t.Parallel()
+
+	m := hashmap.HashMap{}
+
+	err := m.Set("Hello", "World!")
+	assert.NoError(t, err)
+}
