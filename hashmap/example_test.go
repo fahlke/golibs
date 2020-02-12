94faa1615a4f7b4b5851aab9fcdf3b559d3a7759
Alexander Fahlke <alexander.fahlke+git@gmail.com>
2020-02-12 15:37:20 +0100

    #8 added examples for HashMap

--- hashmap/example_test.go
+++ hashmap/example_test.go
@@ -1,1 +1,37 @@
 package hashmap_test
+
+import (
+	"fmt"
+
+	"github.com/fahlke/golibs/hashmap"
+)
+
+func ExampleHashMap_Set() {
+	hm := hashmap.HashMap{}
+	hm.Set("foo", "bar")
+}
+
+func ExampleHashMap_Get() {
+	hm := hashmap.HashMap{}
+	hm.Set("foo", "bar")
+
+	value, err := hm.Get("foo")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(value)
+	// output:
+	// bar
+}
+
+func ExampleHashMap_Delete() {
+	hm := hashmap.HashMap{}
+
+	err := hm.Delete("foo")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// output:
+	// item not found
+}
