d0b057d55a74249e9fc199b6872627c3f76210f7
Alexander Fahlke <alexander.fahlke+git@gmail.com>
2020-02-13 14:18:07 +0100

    #6 added documentation for the Stack operations

--- stack/stack.go
+++ stack/stack.go
@@ -10,9 +10,10 @@ type Stack struct {
 	items []interface{}
 }
 
-// ErrEmptyStack returns on an invalid operation on an empty stack
+// ErrEmptyStack will be returned on an invalid operation on an empty stack.
 var ErrEmptyStack = errors.New("empty stack")
 
+// Empty returns true if the underlying stack is empty, false otherwise.
 func (s *Stack) Empty() bool {
 	s.RLock()
 	defer s.RUnlock()
@@ -20,6 +21,7 @@ func (s *Stack) Empty() bool {
 	return len(s.items) == 0
 }
 
+// Size returns the number of elements in the underlying stack.
 func (s *Stack) Size() uint {
 	s.RLock()
 	defer s.RUnlock()
@@ -27,6 +29,7 @@ func (s *Stack) Size() uint {
 	return uint(len(s.items))
 }
 
+// Push is putting the given element value to the top of the stack.
 func (s *Stack) Push(item interface{}) {
 	s.Lock()
 	defer s.Unlock()
@@ -34,6 +37,7 @@ func (s *Stack) Push(item interface{}) {
 	s.items = append(s.items, item)
 }
 
+// Top returns the top element in the stack without removing it. That is the most recently pushed element.
 func (s *Stack) Top() (interface{}, error) {
 	s.RLock()
 	defer s.RUnlock()
@@ -45,6 +49,7 @@ func (s *Stack) Top() (interface{}, error) {
 	return s.items[len(s.items)-1], nil
 }
 
+// Pop removes the top element from the stack and returns it. It returns an error if the stack is empty.
 func (s *Stack) Pop() (interface{}, error) {
 	s.Lock()
 	defer s.Unlock()
