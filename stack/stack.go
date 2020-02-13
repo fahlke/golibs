5bc2306bfffeddc00ef749e56335823b07f3f1be
Alexander Fahlke <alexander.fahlke+git@gmail.com>
2020-02-13 13:02:25 +0100

    #6 fixed missing locking

--- stack/stack.go
+++ stack/stack.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Stack struct {
-	sync.Mutex
+	sync.RWMutex
 	items []interface{}
 }
 
@@ -14,20 +14,32 @@ type Stack struct {
 var ErrEmptyStack = errors.New("empty stack")
 
 func (s *Stack) Empty() bool {
+	s.RLock()
+	defer s.RUnlock()
+
 	return len(s.items) == 0
 }
 
 func (s *Stack) Size() uint {
+	s.RLock()
+	defer s.RUnlock()
+
 	return uint(len(s.items))
 }
 
 func (s *Stack) Push(item interface{}) error {
+	s.Lock()
+	defer s.Unlock()
+
 	s.items = append(s.items, item)
 
 	return nil
 }
 
 func (s *Stack) Top() (interface{}, error) {
+	s.RLock()
+	defer s.RUnlock()
+
 	if s.Empty() {
 		return nil, ErrEmptyStack
 	}
@@ -35,4 +47,9 @@ func (s *Stack) Top() (interface{}, error) {
 	return s.items[len(s.items)-1], nil
 }
 
-func (s *Stack) Pop() (interface{}, error) { return nil, nil }
+func (s *Stack) Pop() (interface{}, error) {
+	s.Lock()
+	defer s.Unlock()
+
+	return nil, nil
+}
