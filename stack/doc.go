87f9a38d8cb66ef965adcc5198662a89b121eec7
Alexander Fahlke <alexander.fahlke+git@gmail.com>
2020-02-13 21:47:54 +0100

    #6 added license info

--- stack/doc.go
+++ stack/doc.go
@@ -1,3 +1,7 @@
+// Copyright 2020 Alexander Fahlke.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
 /*
 Package stack provides a simple implementation of a concurrency safe stack.
 */
