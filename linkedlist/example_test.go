294c013f7c6db2c4651a0efe60c6e0b23a91709a
Alexander Fahlke <alexander.fahlke+git@gmail.com>
2020-02-17 20:30:03 +0100

    #10 added a few examples for the linkedlist

--- linkedlist/example_test.go
+++ linkedlist/example_test.go
@@ -1,1 +1,62 @@
 package linkedlist_test
+
+import (
+	"fmt"
+
+	"github.com/fahlke/golibs/linkedlist"
+)
+
+func ExampleLinkedList_Append() {
+	list := linkedlist.LinkedList{}
+
+	list.Append("foo")
+	list.Append("bar")
+	list.Append("baz")
+	// Output:
+}
+
+func ExampleLinkedList_Iterate() {
+	list := linkedlist.LinkedList{}
+
+	list.Append("foo")
+	list.Append("bar")
+	list.Append("baz")
+
+	for node := range list.Iterate() {
+		fmt.Println(node)
+	}
+	// Output:
+	// foo
+	// bar
+	// baz
+}
+
+func ExampleLinkedList_GetNth() {
+	list := linkedlist.LinkedList{}
+
+	list.Append("foo")
+	list.Append("bar")
+	list.Append("baz")
+
+	node, _ := list.GetNth(1)
+	fmt.Println(node)
+	// Output:
+	// bar
+}
+
+func ExampleLinkedList_RemoveBeginning() {
+	list := linkedlist.LinkedList{}
+
+	list.Append("foo")
+	list.Append("bar")
+	list.Append("baz")
+
+	list.RemoveBeginning()
+
+	for node := range list.Iterate() {
+		fmt.Println(node)
+	}
+	// Output:
+	// bar
+	// baz
+}
