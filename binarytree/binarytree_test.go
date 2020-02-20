dd4f9c352e9dffa6b79c4fb64a1eee2d4d9386f6
Alexander Fahlke <alexander.fahlke+git@gmail.com>
2020-02-20 23:09:09 +0100

    #9 added missing update tests for Set()

--- binarytree/binarytree_test.go
+++ binarytree/binarytree_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 //nolint:gochecknoglobals
-var golden = []Item{
+var insertTests = []Item{
 	{"Alpha", "álfa (άλφα)"},
 	{"Beta", "víta (βήτα)"},
 	{"Gamma", "gám(m)a (γάμ(μ)α)"},
@@ -16,25 +16,44 @@ var golden = []Item{
 	{"Zeta", "zíta (ζήτα)"},
 }
 
+//nolint:gochecknoglobals
+var updateTests = []Item{
+	{"Alpha", "álfa (άλφα) (updated)"},
+	{"Delta", "délta (δέλτα) (updated)"},
+}
+
 func TestBinaryTree_Set(t *testing.T) {
 	t.Parallel()
 
 	bt := BinaryTree{}
 
-	for _, item := range golden {
-		bt.Set(item.Key, item.Value)
-	}
+	t.Run("insert", func(t *testing.T) {
+		for _, item := range insertTests {
+			bt.Set(item.Key, item.Value)
+		}
 
-	assert.Equal(t, "Alpha", bt.root.key)
-	assert.Equal(t, "álfa (άλφα)", bt.root.value)
-	assert.Equal(t, "Beta", bt.root.right.key)
-	assert.Equal(t, "víta (βήτα)", bt.root.right.value)
-	assert.Equal(t, "Gamma", bt.root.right.right.key)
-	assert.Equal(t, "gám(m)a (γάμ(μ)α)", bt.root.right.right.value)
-	assert.Equal(t, "Delta", bt.root.right.right.left.key)
-	assert.Equal(t, "délta (δέλτα)", bt.root.right.right.left.value)
-	assert.Equal(t, "Epsilon", bt.root.right.right.left.right.key)
-	assert.Equal(t, "épsilon (έψιλον)", bt.root.right.right.left.right.value)
-	assert.Equal(t, "Zeta", bt.root.right.right.right.key)
-	assert.Equal(t, "zíta (ζήτα)", bt.root.right.right.right.value)
+		assert.Equal(t, "Alpha", bt.root.key)
+		assert.Equal(t, "álfa (άλφα)", bt.root.value)
+		assert.Equal(t, "Beta", bt.root.right.key)
+		assert.Equal(t, "víta (βήτα)", bt.root.right.value)
+		assert.Equal(t, "Gamma", bt.root.right.right.key)
+		assert.Equal(t, "gám(m)a (γάμ(μ)α)", bt.root.right.right.value)
+		assert.Equal(t, "Delta", bt.root.right.right.left.key)
+		assert.Equal(t, "délta (δέλτα)", bt.root.right.right.left.value)
+		assert.Equal(t, "Epsilon", bt.root.right.right.left.right.key)
+		assert.Equal(t, "épsilon (έψιλον)", bt.root.right.right.left.right.value)
+		assert.Equal(t, "Zeta", bt.root.right.right.right.key)
+		assert.Equal(t, "zíta (ζήτα)", bt.root.right.right.right.value)
+	})
+
+	t.Run("update", func(t *testing.T) {
+		for _, item := range updateTests {
+			bt.Set(item.Key, item.Value)
+		}
+
+		assert.Equal(t, "Alpha", bt.root.key)
+		assert.Equal(t, "álfa (άλφα) (updated)", bt.root.value)
+		assert.Equal(t, "Delta", bt.root.right.right.left.key)
+		assert.Equal(t, "délta (δέλτα) (updated)", bt.root.right.right.left.value)
+	})
 }
